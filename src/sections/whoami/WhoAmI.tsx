b48b52d964428e26bab5f73637d29e9c27c852b0
Luigi Perotti <luigi.perotti@redealumni.com>
2019-03-20 19:20:48 -0300

    Adds appear but its not working

--- src/sections/whoami/WhoAmI.tsx
+++ src/sections/whoami/WhoAmI.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import { Slide, Text, Heading, Code, Image, Fit } from 'spectacle';
+import { Slide, Text, Heading, Code, Appear } from 'spectacle';
 
 import LuigiPerotti from '../../assets/images/luigi-perotti.jpg';
 
 export default class WhoAmI extends React.Component {
   public render() {
     return (
-      <Slide>
+      <Slide bgImage={LuigiPerotti} bgDarken={0.8}>
         <Heading>
-          <Code>$ whoami</Code>
+          <Code textColor="#fff">$ whoami</Code>
         </Heading>
-
-        <Fit>
-          <Image src={LuigiPerotti} height={100} />
-        </Fit>
-        <Text>Luigi Perotti</Text>
-        <Text>Desenvolvedor</Text>
-        <Text>Quero Educação - 6 meses</Text>
-        <Text>...e contando</Text>
+        <Text textColor="#fff">Luigi Perotti</Text>
+        <Appear order={0}>
+          <Text textColor="#fff">Desenvolvedor</Text>
+        </Appear>
+        <Appear order={1}>
+          <Text textColor="#fff">Quero Educação - 6 meses</Text>
+        </Appear>
+        <Appear order={2}>
+          <Text textColor="#fff">...e contando</Text>
+        </Appear>
       </Slide>
     );
   }
