4d6653a0e8b7251baa60fa90670356e3e96031d9
Luigi Perotti <luigi.perotti@redealumni.com>
2019-03-22 19:30:56 -0300

    Adds diff example

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,6 @@
 import { configure } from '@storybook/react';
 import 'loki/configure-react';
 
-// automatically import all files ending in *.stories.tsx
 const req = require.context('../src', true, /.stories.tsx$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
---
+++ storybook-static/main.b9275159175efc39a62f.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{392:function(n,o,p){p(393),p(470),n.exports=p(840)},470:function(n,o,p){"use strict";p.r(o);p(471)}},[[392,1,2]]]);
